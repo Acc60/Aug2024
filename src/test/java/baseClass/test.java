db70a71c223e8f1a08dd635607326a4a6f9f0658
Nitish <NITKMR.1996@GMAIL.COM>
2025-03-23 13:09:46 +0530

    First commit for Aug 2024 Project

---
+++ src/test/java/baseClass/test.java
@@ -0,0 +1,16 @@
+package baseClass;
+
+public class test {
+	
+	void tests(){
+		System.out.println("Printing first line in this project Aug 2024");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       test t=new test();
+       t.tests();
+	}
+
+}
